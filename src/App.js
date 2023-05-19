fc54503574f1ac3eb93224c44471a58cc86175dd
pavankumarreddy <pavankumareddy635@gmail.com>
2023-05-19 15:54:18 +0530

    push the froent end code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [data, setData] = useState({ members: [] });
+  const [data, setData] = useState({ students: [] });
 
   useEffect(() => {
-    fetch('/members')
+    fetch('/students')
       .then(res => res.json())
       .then(data => {
         setData(data);
@@ -15,12 +15,29 @@ function App() {
 
   return (
     <div>
-      {data.members.length === 0 ? (
+      {data.students.length === 0 ? (
         <p>Loading...</p>
       ) : (
-        data.members.map((member, i) => (
-          <p key={i}>{member}</p>
-        ))
+        <table style={{ border: '1px solid black', borderCollapse: 'collapse' }}>
+          <thead>
+            <tr>
+              <th style={{ border: '1px solid black', padding: '5px' }}>Sno</th>
+              <th style={{ border: '1px solid black', padding: '5px' }}>Name</th>
+              <th style={{ border: '1px solid black', padding: '5px' }}>Course</th>
+              <th style={{ border: '1px solid black', padding: '5px' }}>Age</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.students.map((student, i) => (
+              <tr key={i}>
+                <td style={{ border: '1px solid black', padding: '5px' }}>{student.Sno}</td>
+                <td style={{ border: '1px solid black', padding: '5px' }}>{student.Name}</td>
+                <td style={{ border: '1px solid black', padding: '5px' }}>{student.course}</td>
+                <td style={{ border: '1px solid black', padding: '5px' }}>{student.Age}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       )}
     </div>
   );
